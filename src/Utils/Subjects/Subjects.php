9f9ff31ae21a7076068f5e5082b91cbd6251eaf5
Sofiosko <mar.konderla@gmail.com>
2019-06-19 12:07:30 +0200

    subjects init

--- src/Utils/Subjects/Subjects.php
+++ src/Utils/Subjects/Subjects.php
@@ -6,127 +6,92 @@ class Subjects
     const COUNTRY_CZ = 'cz';
     const COUNTRY_SK = 'sk';
 
+    const SUBJECTS_API_URL = 'http://subjects.biteit.cz/api';
+
     protected $license;
     protected $cacheTime = 3600;
+    protected $cacheFolder;
 
-    public function __construct($license, $cacheTime = null)
+    public function __construct($license)
     {
         $this->license = $license;
+    }
 
+    /**
+     * @param $cacheFolder
+     * @param null $cacheTime
+     * @return $this
+     */
+    public function setCache($cacheFolder, $cacheTime = null){
         if(isset($cacheTime))
             $this->cacheTime = $cacheTime;
+
+        $this->cacheFolder = $cacheFolder;
+        return $this;
     }
 
+    /**
+     * @param string $companyCode
+     * @param string $countryCode
+     * @return bool|null
+     * @throws \Exception
+     */
     public function getCompanyInfo(string $companyCode, $countryCode = Subjects::COUNTRY_CZ){
-        if($countryCode == static::COUNTRY_CZ){
-            $subject = $this->_getCZCompanyInfo($companyCode);
+        $res = $this->callApi('get-info', [
+            'cc' => $countryCode,
+            'number' => $companyCode,
+            'key' => $this->license
+        ]);
+
+        if($res){
+            $res = json_decode($res);
+            if(isset($res->info))
+                return $res->info;
+            return false;
         } else {
-            $subject = $this->_getSKCompanyInfo($companyCode);
+            return null;
         }
-
-        var_dump($subject);
     }
 
+    /**
+     * @param string $taxNumber
+     * @param string $countryCode
+     * @return bool|null
+     * @throws \Exception
+     */
     public function validateTaxNumber(string $taxNumber, $countryCode = Subjects::COUNTRY_CZ){
+        $res = $this->callApi('validate-tax-number', [
+            'cc' => $countryCode,
+            'number' => $taxNumber,
+            'key' => $this->license
+        ]);
+
+        if($res){
+            return boolval(json_decode($res)->success);
+        } else {
+            return null;
+        }
     }
 
+    /**
+     * @param string $companyCode
+     * @param string $countryCode
+     * @return bool|null
+     * @throws \Exception
+     */
     public function validateCompanyCode(string $companyCode, $countryCode = Subjects::COUNTRY_CZ){
+        $res = $this->callApi('validate-company-code', [
+            'cc' => $countryCode,
+            'number' => $companyCode,
+            'key' => $this->license
+        ]);
 
-    }
-
-    protected function _getCZCompanyInfo(string $companyCode){
-        $response = new SubjectsResponse();
-
-        $file = @file_get_contents(Helpers::ARES_URL . $companyCode);
-        if ($file) $xml = @simplexml_load_string($file);
-        $a = array();
-        if ($xml)
-        {
-            $ns = $xml->getDocNamespaces();
-            $data = $xml->children($ns['are']);
-            $el = $data->children($ns['D'])->VBAS;
-            if (strval($el->ICO) == $companyCode)
-            {
-                $a['ico'] = strval($el->ICO);
-                $a['dic'] = strval($el->DIC);
-                $a['firma'] = strval($el->OF);
-                $a['ulice'] = strval($el->AA->NU);
-                if (!empty($el->AA->CO) OR !empty($el->AA->CD))
-                {
-                    // detekování popisného a orientačního čísla
-                    $a['ulice'] .= " ";
-                    if (!empty($el->AA->CD)) $a['ulice'] .= strval($el->AA->CD);
-                    if (!empty($el->AA->CO) AND !empty($el->AA->CD)) $a['ulice'] .= "/";
-                    if (!empty($el->AA->CO)) $a['ulice'] .= strval($el->AA->CO);
-                }
-                // je fyzicka osoba?
-                $a['fyz'] = strpos(strval($el->PF->NPF), 'yzick') !== false ? true : false;
-                $a['mesto'] = strval($el->AA->N);
-                $a['psc'] = strval($el->AA->PSC);
-                $a['stav'] = 1;
-
-            } else
-                $a['stav'] = 'IČ firmy nebylo nalezeno. Prosím, ověřte správnost čísla a případně zadejte znovu.';
-        } else
-            $a['stav'] = 'Databáze ARES není dostupná';
-
-        return $response;
-    }
-
-    protected function _getSKCompanyInfo(string $companyCode){
-        $response = new SubjectsResponse();
-
-        $html = file_get_contents(static::FINSTAT_SK_URL.$companyCode);
-        if($html === false){
-            $a['stav'] = 'IČ nenalezeno.';
-            return $a;
-        }
-        $main = preg_match('/<ul class="ul-list m-b-none">(.+?)<\/ul>/sim', $html, $matches);
-        if(!$matches[1]){
-            $a['stav'] = 'IČ nenalezeno.';
-            return $a;
-        }
-        preg_match_all('/<li.*?>(.+?)<\/li>/sim', $matches[1], $lines);
-        $a = [];
-        $a['ico'] = $companyCode;
-        $a['stav'] = 1;
-        foreach ($lines[1] as $line){
-            preg_match('/<span*.?>(.+?)<\/span>/sim', $line, $dataMatch);
-            $data = html_entity_decode(trim($dataMatch[1]));
-
-            if( strpos($line, 'DIČ') !== false ){
-                $a['dic'] = $data;
-            }
-            else if( stripos($line, 'sídlo') )
-            {
-                $d = explode("\n", $data);
-                $a['firma'] = trim(strip_tags($d[0]));
-                $a['fyz'] = strpos($a['firma'], 's. r. o.') === false && strpos($a['firma'], 's.r.o.') === false &&
-                    strpos($a['firma'], 'a. s.') === false &&
-                    $data = trim($d[1]);
-                preg_match('/\d{3} \d{2} /', $data, $pscMatch);
-                if($pscMatch){
-                    $address = explode($pscMatch[0], $data);
-
-                    $ad = explode("\n", $address[0]);
-                    if(trim(strip_tags($ad[0])) == $a['firma']){
-                        $a['ulice'] = trim($ad[1]);
-                    } else {
-                        $a['ulice'] = trim($address[0]);
-                    }
-
-                    $a['psc'] = $pscMatch[0];
-                    $a['mesto'] = trim($address[1]);
-                }
-            }
-            else if( stripos($line, 'forma') )
-            {
-                $a['fyz'] = stripos($data, 'fyzick') !== false ? 1 : 0;
-            }
-
+        if($res){
+            return boolval(json_decode($res)->success);
+        } else {
+            return null;
         }
 
-        return $response;
     }
 
     /**
@@ -143,5 +108,49 @@ class Subjects
     public function setCacheTime(int $cacheTime): void
     {
         $this->cacheTime = $cacheTime;
+    }
+
+    /**
+     * @param string $endpoint
+     * @param array $params
+     * @param bool $cache
+     * @return false|string
+     */
+    protected function callApi(string $endpoint, array $params, $cache = true){
+        $url = static::SUBJECTS_API_URL.'/'.$endpoint.'?'.http_build_query($params);
+        $file = realpath($this->cacheFolder).'/response-'.md5($url).'.json';
+
+        if($cache){
+            if(!isset($this->cacheFolder))
+                throw new \Exception('Please set cache folder');
+
+            if(!file_exists($this->cacheFolder))
+                mkdir($this->cacheFolder, 0755, true);
+
+            if(file_exists($file) && time() < (filemtime($file) + $this->cacheTime)) {
+                return file_get_contents($file);
+            }
+        }
+
+        $response = file_get_contents($url);
+        if($cache)
+            file_put_contents($file, $response);
+        return $response;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCacheFolder(): string
+    {
+        return $this->cacheFolder;
+    }
+
+    /**
+     * @param string $cacheFolder
+     */
+    public function setCacheFolder(string $cacheFolder): void
+    {
+        $this->cacheFolder = $cacheFolder;
     }
 }
--- tests/test-subjects.php
+++ tests/test-subjects.php
@@ -1,6 +1,12 @@
 <?php
 require_once __DIR__ . '/../src/init.php';
 
-$api = new \BiteIT\Utils\Subjects('j3FdMCUKSSzqwBNg');
+$api = new \BiteIT\Utils\Subjects('key');
+$api->setCache(__DIR__ . '/temp', 10);
 
-$api->getCompanyInfo('e');
+echo '<pre>';
+var_dump([
+    $api->validateCompanyCode('05987059'),
+    $api->validateTaxNumber('05987059'),
+    $api->getCompanyInfo('05987059'),
+]);
